ab13f4aa9633c417d0e26a475eafe4f2203dccd6
ABINESH <abineshms7@gmail.com>
2026-03-20 21:48:48 +0530

    added origin url

--- backend_python/main.py
+++ backend_python/main.py
@@ -9,10 +9,12 @@ from routes.resume_builder_routes import router as resume_builder_router
 
 app = FastAPI()
 
-
+origins = [
+    "https://agent-market-frontend-three.vercel.app"
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
